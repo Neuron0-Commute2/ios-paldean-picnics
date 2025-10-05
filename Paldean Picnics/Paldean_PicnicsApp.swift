b5d8b6de0ac027e66e7fa777c2b88679389575cf
Penelope Kyber <tower.relight003@passmail.net>
2025-10-04 19:52:45 -0700

    Initial Commit

---
+++ Paldean Picnics/Paldean_PicnicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Paldean_PicnicsApp.swift
+//  Paldean Picnics
+//
+//  Created by Penelope on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Paldean_PicnicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
